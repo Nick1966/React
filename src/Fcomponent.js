31380657573556b86a8d0f9ded5bb9a3445a60a9
Nick1966 <nvakoliouk@gmail.com>
2020-12-26 00:16:18 -0800

    Initialize project using Create React App

--- src/Fcomponent.js
+++ src/Fcomponent.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Fcomponent(props) {
     return (
         <div>
-            <h1>Hello 123, {props.name}</h1>
+            <h1>Hello, Nick 1966 {props.name}</h1>
         </div>
     )
 }
